9d1d27b7929ed6bd8125cc56d4a2be0bcac5271d
Denis S <makafonov@gmail.com>
2022-10-01 13:22:29 +0500

    Footer, Main

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import './Footer.css';
+
+const Footer = () => (
+  <footer className='footer'>
+    <div className='footer__container'>
+      <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__info'>
+        <p className='footer__copyright'>&copy; 2022</p>
+        <ul className='footer__links'>
+          <li>
+            <a
+              className='footer__link'
+              href='https://practicum.yandex.ru'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li>
+            <a
+              className='footer__link'
+              href='https://github.com/makafonov'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
